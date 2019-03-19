efc1545db596f5b8a3618cdebe443d16bb1d63cd
RANJITHKUMAR12345 <36396701+RANJITHKUMAR12345@users.noreply.github.com>
2019-03-19 02:48:47 -0700

    Create beginset61.c

---
+++ beginset61.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+int x,y,*a,*b,sum;
+clrscr();
+printf("Enter two numbers to be added:");
+scanf("%d%d",&x,&y);
+a=&x;
+b=&y;
+sum=*a+*b;
+printf("Addition is:%d",sum);
+}
